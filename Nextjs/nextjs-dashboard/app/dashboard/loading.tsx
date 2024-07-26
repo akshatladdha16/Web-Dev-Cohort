5d9daa9d5049bfcdedd6ebef932d86b9ebedd147
Akshat Rai Laddha <laddhaakshatrai@gmail.com>
2024-07-26 14:16:46 +0530

    streaming concept

---
+++ Nextjs/nextjs-dashboard/app/dashboard/loading.tsx
@@ -0,0 +1,6 @@
+import DashboardSkeleton from "../ui/skeletons"
+
+export default function Loading(){
+    return <DashboardSkeleton />
+}
+//this logic will work on customers and invoices pages as in file system it is above them 
--- Nextjs/nextjs-dashboard/app/dashboard/page.tsx
+++ Nextjs/nextjs-dashboard/app/dashboard/page.tsx
@@ -1,11 +1,14 @@
 import { Card } from '@/app/ui/dashboard/cards';
-import RevenueChart from '@/app/ui/dashboard/revenue-chart';
 import LatestInvoices from '@/app/ui/dashboard/latest-invoices';
 import { lusitana } from '@/app/ui/fonts';
+import RevenueChart from '@/app/ui/dashboard/revenue-chart';
+import { Suspense } from 'react';
+import { RevenueChartSkeleton } from '../ui/skeletons';
+
 // fetching data components
-import { fetchRevenue,fetchLatestInvoices,fetchCardData } from '../lib/data';
+import { fetchLatestInvoices,fetchCardData } from '../lib/data';
 export default async function Page() {
-    const revenue=await fetchRevenue();
+    
     const latestInvoices=await fetchLatestInvoices();
     const {numberOfCustomers,
         numberOfInvoices,
@@ -27,7 +30,11 @@ export default async function Page() {
         />
       </div>
       <div className="mt-6 grid grid-cols-1 gap-6 md:grid-cols-4 lg:grid-cols-8">
-        <RevenueChart revenue={revenue}  />
+
+        <Suspense fallback={<RevenueChartSkeleton/>}>
+        <RevenueChart/>
+        </Suspense>
+
         <LatestInvoices latestInvoices={latestInvoices} />
       </div>
     </main>
